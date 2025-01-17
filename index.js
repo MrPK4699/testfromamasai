c04b60fc75a42a06262fb6de0392b31b8fd66ac1
Pankaj Khatri <4699pankajkhatri@gmail.com>
2025-01-17 22:49:20 +0530

    middleware is added for json parsing

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { route } = require('./routes/route');
+const findUniqCharRoute = require('./routes/findUniqCharRoute');
 const app = express();
 
+app.use(express.json());
 
-app.use('/',route);
+app.use('/', findUniqCharRoute);
 
 const port=5000;
 app.listen(port || 5000, () => {
       console.log('server is running at the port 5000')
 })
-
 
